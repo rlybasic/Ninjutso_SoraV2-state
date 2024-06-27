666a7f0060046fb40e6e5aa7abbdde0339fddb44
Basic <vlad04042006@gmail.com>
2024-06-27 21:16:14 +0300

    Done

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Ninjutso_SoraV2-state
-Get information about Ninjutso Sora v2 mouse using hidapi
+### Get information about Ninjutso Sora v2 mouse using hidapi
+
+## The idea for the realisation was taken from [Sora_tray](https://github.com/Fan4Metal/Sora_tray/)
--- main.c
+++ main.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 #include <hidapi.h>
 
+#ifdef _WIN32
+
+#include <Windows.h>
+
+#else
+
+#include <unistd.h>
+
+#endif
+
 #define VID 0x1915
 #define PID_WIRELESS 0xAE1C
 #define PID_WIRED 0xAE11
 #define USAGE_PAGE 0xFFA0
+
+struct BatteryInfo {
+    int iBattery;
+    int bCharging;
+    int bFullCharge;
+    int bOnline;
+};
 
 hid_device *GetDevice() {
     struct hid_device_info *psDevs = NULL;
@@ -41,7 +58,82 @@ hid_device *GetDevice() {
     return phDevice;
 }
 
-int main(void) {
-    hid_device *hid_device = GetDevice();
+struct BatteryInfo GetBattery() {
+    struct BatteryInfo sInfo = {0, 0, 0, 0};
+    hid_device *phDevice = GetDevice();
+
+    if (!phDevice) {
+        printf("Failed to get device\n");
+        return sInfo;
+    }
+
+    unsigned char acReport[32] = {0};
+    acReport[0] = 5;  // Report ID
+    acReport[1] = 21;
+    acReport[4] = 1;
+
+    printf("Sending report\n");
+    for (int i = 0; i < 32; i++) {
+        printf("%02x ", acReport[i]);
+    }
+    printf("\n");
+    int iRes = hid_send_feature_report(phDevice, acReport, sizeof(acReport));
+    if (iRes == -1) {
+        printf("Failed to send feature report\n");
+        hid_close(phDevice);
+        return sInfo;
+    }
+#ifdef _WIN32
+    Sleep(1000);
+#else
+    sleep(1);
+#endif
+
+    unsigned char acResponse[32] = {0};
+    acResponse[0] = 5;  // Set the report ID in the buffer
+
+    iRes = hid_get_feature_report(phDevice, acResponse, sizeof(acResponse));
+    if (iRes == -1) {
+        printf("Failed to get feature report\n");
+        hid_close(phDevice);
+        return sInfo;
+    }
+
+    printf("Received report, bytes read: %d\n", iRes);
+
+    // Print the raw response for debugging
+    printf("Raw response:\n");
+    for (int i = 0; i < iRes; i++) {
+        printf("%02x ", acResponse[i]);
+    }
+    printf("\n");
+
+    if (iRes >= 13) {  // Ensure we have enough data
+        sInfo.iBattery = acResponse[9];
+        sInfo.bCharging = acResponse[10] != 0;
+        sInfo.bFullCharge = acResponse[11] != 0;
+        sInfo.bOnline = acResponse[12] != 0;
+
+    } else {
+        printf("Insufficient data in response\n");
+    }
+
+    hid_close(phDevice);
+    return sInfo;
+}
+
+int main() {
+    if (hid_init() != 0) {
+        printf("Failed to initialize hidapi");
+        return 1;
+    }
+
+    struct BatteryInfo sInfo = GetBattery();
+    printf("Battery: %d\n", sInfo.iBattery);
+    printf("Charging: %d\n", sInfo.bCharging);
+    printf("Full charge: %d\n", sInfo.bFullCharge);
+    printf("Online: %d\n", sInfo.bOnline);
+
+    hid_exit();
     return 0;
 }
